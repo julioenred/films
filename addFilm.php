8e6db840b18536ecec1b2d8180558a05c962eb92
julioperezdecastrovanadis <julio.perezdecastro@vanadis.es>
2021-03-01 22:41:31 +0100

    log error

--- addFilm.php
+++ addFilm.php
@@ -6,5 +6,6 @@ $conn = new mysqli($servername, $username, $password, $db);
 
 $sql = "INSERT INTO Films (name, description) VALUES ('" . $_POST['name'] . "', '" . $_POST['description'] . "')";
 $conn->query($sql);
+var_dump($conn->error); 
 
 var_dump('success'); exit;
